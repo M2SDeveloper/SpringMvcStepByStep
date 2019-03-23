356619b194a032a51e01ed329eebd2a3f42cb33c
Mangesh_Sawant <mangesh2707@gmail.com>
2019-03-23 21:08:49 +0530

    Spring Auto-wiring and Dependency Management - @Autowired and @Service

--- src/main/java/com/m2sdeveloper/login/LoginService.java
+++ src/main/java/com/m2sdeveloper/login/LoginService.java
@@ -1,5 +1,8 @@
 package com.m2sdeveloper.login;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
 
 	public boolean validateUser(String name, String password)
