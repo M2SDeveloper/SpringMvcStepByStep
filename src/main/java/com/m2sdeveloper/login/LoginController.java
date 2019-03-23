fb5ee8e5de7a700fe40ada5c2328bbb399361543
Mangesh_Sawant <mangesh2707@gmail.com>
2019-03-23 20:59:16 +0530

    LoginService and Remove all JEE Servlets based code

--- src/main/java/com/m2sdeveloper/login/LoginController.java
+++ src/main/java/com/m2sdeveloper/login/LoginController.java
@@ -8,20 +8,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-	
-	@RequestMapping(value="/login", method=RequestMethod.GET)
-	public String showLoginPage()
-	{
+
+	private LoginService loginService = new LoginService();
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String showLoginPage() {
 		System.out.println("Hello World");
 		return "welcome";
 	}
-	
-	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String handleUserLogin(ModelMap model, @RequestParam String name,@RequestParam String password)
-	{
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String handleUserLogin(ModelMap model, @RequestParam String name, @RequestParam String password) {
+		if (!loginService.validateUser(name, password)) {
+			model.put("errorMessage", "Invalid Credentials");
+			return "login";
+		}
+
 		System.out.println("Hello World");
 		model.put("name", name);
-		model.put("password",password);
 		return "welcome";
 	}
 }
