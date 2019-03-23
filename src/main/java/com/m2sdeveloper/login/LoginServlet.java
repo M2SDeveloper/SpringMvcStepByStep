e9782dd7006100e677418ab888ad4d1c138f793a
Mangesh_Sawant <mangesh2707@gmail.com>
2019-03-23 11:56:32 +0530

    Adding another Get Parameter Password

--- src/main/java/com/m2sdeveloper/login/LoginServlet.java
+++ src/main/java/com/m2sdeveloper/login/LoginServlet.java
@@ -40,6 +40,7 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("In Get method of login servlet");
 		request.setAttribute("name", request.getParameter("name"));
+		request.setAttribute("password", request.getParameter("password"));
 		request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request, response);
 	}
 
